e0484a42864335cb2d9ce6f927da822104a334f9
Akshay Somayaji <akshaysomayaji@gmail.com>
2025-10-29 22:36:12 +0530

    dashboard added

--- app/controllers/sellerdashboard.server.controller.js
+++ app/controllers/sellerdashboard.server.controller.js
@@ -2,7 +2,9 @@ const { where, Op } = require("sequelize");
 const db = require("../../config/plsql");
 const users = db.userdetails;
 const productdetails = db.productDetails;
-const admindashboardresponse = {
+const orderedItems = db.orderedItems;
+const businessDetails = db.businessDetails;
+const sellerdashboardresponse = {
     loggedInUserName: {
         fullName: '',
         pan: '',
@@ -17,19 +19,57 @@ const admindashboardresponse = {
 };
 
 exports.getdashboardcontent = async function (req, res, next) {
-
+    const id = req.decoded.id;
+    getContent(id);
+    getUserDetails(id);
+    return res.send({ data: sellerdashboardresponse, success: true, response_message: "" });
 }
 
-function getUserDetails() {
-    const id = req.decoded.id;
+function getUserDetails(id) {
+
     users.findByPk(id).then(result => {
-        if (!result) {
+        if (result) {
+            console.log(result.dataValues);
             const userdetails = result.dataValues;
-            admindashboardresponse.loggedInUserName.fullName = userdetails.fullname;
-            admindashboardresponse.loggedInUserName.pan = userdetails.fullname;
-        res.send({ data: result, success: true, msg: "" });
+            sellerdashboardresponse.loggedInUserName.fullName = userdetails.emailid;
+            
+        }
+    }).catch(err => {
+        sellerdashboardresponse.loggedInUserName.fullName = "";
+    });
+
+    businessDetails.findOne({ userId: id, isActive: true }).then(response => {
+        sellerdashboardresponse.loggedInUserName.pan = response.dataValues.pan;
+    }).catch(err => {
+        sellerdashboardresponse.loggedInUserName.pan = "";
+    });
+}
+
+function getContent(id) {
+    productdetails.count({ where: { createdUser: id, isActive: true } }).then(res => {
+        sellerdashboardresponse.totalProducts = res;
+    }).catch(err => {
+        sellerdashboardresponse.totalProducts = 0;
+    });
+
+    var condtion = { where: { sellerId: id, paymentStatus: 'PAID', status: 'Ordered' } };
+    orderedItems.count(condtion).then(respose => {
+        sellerdashboardresponse.totalProducts = respose;
+    }).catch(err => {
+        sellerdashboardresponse.totalProducts = 0;
     })
-        .catch(err => {
-            res.status(500).send({ data: {}, success: false, msg: notification.getUser_notification_message('User000'), err });
-        });
+
+    var condtion = { where: { sellerId: id, paymentStatus: 'PAID', status: 'Delivered' } };
+    orderedItems.count(condtion).then(respose => {
+        sellerdashboardresponse.ordersDelivered = respose;
+    }).catch(err => {
+        sellerdashboardresponse.ordersDelivered = 0;
+    })
+
+    var condtion = { where: { sellerId: id, paymentStatus: 'PAID', status: 'Returned' } };
+    orderedItems.count(condtion).then(respose => {
+        sellerdashboardresponse.ordersReturned = respose;
+    }).catch(err => {
+        sellerdashboardresponse.ordersReturned = 0;
+    })
 }
--- app/routes/maindashboard.server.routes.js
+++ app/routes/maindashboard.server.routes.js
@@ -1,6 +1,8 @@
 const dashboardCtrl = require('../../app/controllers/admindashboard.server.controller');
+const sellerdashboardCtrl = require('../../app/controllers/sellerdashboard.server.controller');
 const { authenticate, authorizeRoles } = require("../helpers/authorizationHelper");
 module.exports = function (app) {
-    app.route('/api/dashboard/admin').get(authorizeRoles("ADMIN"),dashboardCtrl.getdashboardcontent);
+    app.route('/api/dashboard/admin').get(authorizeRoles("ADMIN"), dashboardCtrl.getdashboardcontent);
+    app.route('/api/dashboard/seller').get(authorizeRoles("SELLER"), sellerdashboardCtrl.getdashboardcontent);
 }
 
